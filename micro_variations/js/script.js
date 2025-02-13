1c4d873700288bb424f0c7f18d120bc06327ac4d
nolwennhrv <nolwenn.herve@ensaama.net>
2025-02-13 17:33:15 +0100

    Add files via upload

---
+++ micro_variations/js/script.js
@@ -0,0 +1,45 @@
+var positionY;
+var scrollTop;
+
+var lastPositionX =0;
+
+$( document ).ready(function() {
+    //récupérer les dimension de la fenêtre :
+    var hauteurFenetre = $(window).width();
+    var largeurFenetre = $(window).height();
+    scrollTop = $(window).scrollTop(); //récupérer la "quantité" de scroll
+    
+////vous pouvez rédiger vos modification ici !
+
+$( ".ronds img" ).on( "mousemove", function() {
+  
+  $(this).css({
+   "scale" : ""+event.pageX*0.005+""
+    });
+});
+
+$( ".ronds img" ).on( "mouseleave", function() {
+  
+  $(this).css({
+   "scale" : "1"
+    });
+});
+
+/////Ci-dessous la fonction permettant de déclancher des modifications en fonction du scroll
+$(window).on( 'scroll', function(){
+    scrollTop = $(window).scrollTop(); //création d'une varible contenant la "quantitée de scroll"
+ });
+
+
+
+  $(window).on('mousemove', function(){
+    positionY = event.pageY - scrollTop; // corection de la valeur de event.pageY pour qu'elle ne soit pas affectée par le scroll
+  })
+
+
+
+ //////// ///ne rien écrire après ceci
+});
+
+
+
